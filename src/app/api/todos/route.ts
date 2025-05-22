fa2a758ec5dcf71ea8715a78cefc110934e3fb96
Gareth Houghton <ghoughton@vendavo.com>
2025-05-22 11:43:01 +0100

    added api routes and updated readme:

---
+++ src/app/api/todos/route.ts
@@ -0,0 +1,43 @@
+import { db } from "@/drizzle/db";
+import { todos } from "@/drizzle/schema";
+import { eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const allTodos = await db.select().from(todos)
+    return NextResponse.json(allTodos);
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to fetch todos" }, { status: 500 });
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const { title } = await req.json();
+    await db.insert(todos).values({ title, completed: false });
+    return NextResponse.json({ message: "Todo added successfully" });
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to add todo" }, { status: 500 });
+  }
+}
+
+export async function PUT(req: Request) {
+  try {
+    const { id, completed } = await req.json();
+    await db.update(todos).set({ completed }).where(eq(todos.id, id));
+    return NextResponse.json({ message: "Todo updated successfully" });
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to update todo" }, { status: 500 });
+  }
+}
+
+export async function DELETE(req: Request) {
+  try {
+    const { id } = await req.json();
+    await db.delete(todos).where(eq(todos.id, id));
+    return NextResponse.json({ message: "Todo successfully deleted" });
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to delete todo" }, { status: 500 });
+  }
+}
