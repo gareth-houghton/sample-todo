b27887fa9c543ea03923908c22f72f3bada6cdba
Gareth Houghton <ghoughton@vendavo.com>
2025-05-22 14:19:59 +0100

    removed duplicate code from src/drizzle/db.ts

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -2,17 +2,6 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Client } from "pg";
 import { todos } from "./schema";
 
-if(!process.env.PGDB_URL) {
-  throw new Error("Database connection string (PGDB_URL) is not defined");
-}
-
-const client = new Client({
-  connectionString: process.env.PGDB_URL,
-});
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Client } from "pg";
-import { todos } from "./schema";
-
 if (!process.env.PGDB_URL) {
   throw new Error("Database connection string (PGDB_URL) is not defined");
 }
