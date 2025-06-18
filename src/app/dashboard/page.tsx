7794a05be110a19b43bb12f4acb33e6392fb0643
Gareth Houghton <ghoughton@vendavo.com>
2025-06-18 12:29:47 +0100

    fixed mounting

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,80 +1,118 @@
 "use client"
 
 import { embedDashboard } from "@superset-ui/embedded-sdk";
-import axios from "axios";
+import { useRef, useEffect, useState } from "react";
 
 const supersetUrl = "http://localhost:8088";
 const supersetApiUrl = supersetUrl + "/api/v1/security";
-const dashboardId = "8c39a821-b832-4d4c-80e0-20ce8b2e27f2";
+const dashboardId = "61780778-231a-40f4-b6a9-04b8c94c5636";
 
-async function getToken(){
-  const login_body = {
-    "password": "admin",
-    "provider": "db",
-    "refresh": true,
-    "username": "admin"
-  };
-  const login_headers = {
-    "headers": {
-      "Content-Type": "application/json"
-    }
-  };
-
-  const { data } = await axios.post(supersetApiUrl + "/login", login_body, login_headers);
-  const accessToken = data["access_token"];
-  
-  const guest_token_body = JSON.stringify({
-    "resources": [
-      {
-        "type": "dashboard",
-        "id": dashboardId,
-      }
-    ],
-    "rls": [],
-    "user": {
-      "username": "",
-      "first_name": "",
-      "last_name": "",
-    }
-  });
-
-  const guest_token_headers = {
-    "headers": {
-      "Content-Type": "application/json",
-      "Authorization": 'Bearer ' + accessToken
-    }
-  }
-
-  await axios.post(supersetApiUrl + "/guest_token/", guest_token_body, guest_token_headers).then(dt => {
-    embedDashboard({
-      id: dashboardId,
-      supersetDomain: supersetUrl,
-      mountPoint: document.getElementById("superset-container"),
-      fetchGuestToken: () => dt.data["token"],
-      dashboardUiConfig: {
-        filters: {
-          expanded: true
-        },
-        urlParams: {
-          standalone: 3
-        }
-      }
+async function fetchGuestTokenFromBackend() : Promise<string> {
+  try{
+    // Superset Login
+    const apiRes = await fetch(supersetApiUrl + "/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "password": "admin",
+        "provider": "db",
+        "refresh": true,
+        "username": "admin"
+      }),
     });
-  });
+    const { access_token: accessToken } = await apiRes.json();
 
-  var iframe = document.querySelector("iframe");
-  if(iframe) {
-    iframe.style.width = "100%"
-    iframe.style.minHeight = "100vw"
+    // Fetch CSRF token
+    const csrfTokenRes = await fetch(supersetApiUrl + "/csrf_token/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${accessToken}`
+      },
+    });
+    const { result: csrfToken } = await csrfTokenRes.json()
+    const csrfTokenCookies = csrfTokenRes.headers.get("Set-Cookie") || "";
+
+    // Fetch guest token with limited access
+    const guestTokenRes = await fetch(supersetApiUrl + "/guest_token/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${accessToken}`,
+        "X-CSRFToken": csrfToken,
+        "Cookie": csrfTokenCookies
+      },
+      body: JSON.stringify({
+        "resources": [
+          {
+            "id": dashboardId,
+            "type": "dashboard"
+          }
+        ],
+        "rls": [],
+        "user": {
+          "first_name": "guest",
+          "last_name": "guest",
+          "username": "guest"
+        }
+      }),
+    });
+    const { token: guestToken } = await guestTokenRes.json();
+
+    return guestToken;
+  } catch (err) {
+    console.error(err)
+    throw err
   }
 }
 
+export const useSupersetEmbed = (id: string) => {
+  const [mounted, setMounted] = useState(false);
+  const elementRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if ( !elementRef.current ) return;
+    if ( mounted ) return;
+
+    void embedDashboard({
+      id: id,
+      supersetDomain: supersetUrl,
+      mountPoint: elementRef.current,
+      fetchGuestToken: async () => await fetchGuestTokenFromBackend(),
+      dashboardUiConfig: {
+        hideTitle: true,
+        hideChartControls: true,
+        // hideTab: true,
+        filters: {
+          visible: false,
+          
+       }
+      }
+    });
+
+    setMounted(true)
+  }, [id, mounted]);
+
+  return elementRef;
+};
+
 export default function Home(){
-  getToken();
-  
+  const embed = useSupersetEmbed(dashboardId);
+  const parentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const iFrameSuperset = embed.current!.children[0]
+
+    if (iFrameSuperset) {
+
+    }
+  }, [parentRef]);
+
   return (
-    <div className="App">
-      <div id="superset-container"></div>
+    <div className="App w-full h-full" ref={parentRef}>
+      <div className="superset-container w-full h-full" ref={embed}></div>
     </div>
   )
 }
